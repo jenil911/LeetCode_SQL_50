2cfb9021d31718c39a18f0649505ebab8ff44b84
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-02-07 18:26:44 -0500

    Time: 639 ms (41.16%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees e
+WHERE salary < 30000
+AND manager_id IS NOT NULL
+AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
