d55d96143a940bf0fd9ebc3639f3b7f1facadec1
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-03-11 11:05:48 -0400

    Time: 559 ms (58.56%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,7 +1,9 @@
-# Write your MySQL query statement below
-with base as(select requester_id id from RequestAccepted
-union all
-select accepter_id id from RequestAccepted)
-
-
-select id, count(*) num  from base group by 1 order by 2 desc limit 1
+/* Write your T-SQL query statement below */
+SELECT TOP 1 id, COUNT(*) num
+    FROM (SELECT requester_id id
+        FROM RequestAccepted
+    UNION ALL
+        SELECT accepter_id id
+        FROM RequestAccepted) sub
+    GROUP BY id
+    ORDER BY num DESC
