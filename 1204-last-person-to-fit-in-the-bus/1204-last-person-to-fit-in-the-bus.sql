af41547e7bc9da09b108b10bbe27d5ba79509c4b
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-03-16 04:02:14 -0400

    Time: 3186 ms (42.08%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
-SELECT 
-    q1.person_name
-FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
-GROUP BY q1.turn
-HAVING SUM(q2.weight) <= 1000
-ORDER BY SUM(q2.weight) DESC
-LIMIT 1
+/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */
+with cte as (
+    SELECT *,
+        SUM(weight) OVER(ORDER BY turn ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) w_sum
+    FROM queue
+)
+select top 1 person_name from cte where w_sum <= 1000 order by turn desc
