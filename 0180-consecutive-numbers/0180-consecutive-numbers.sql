e0edf01acbfc68d1f3d1fc299927551070d95eaa
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-03-19 18:37:09 +0000

    Time: 704 ms (88.93%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,9 @@
+select
+    distinct l1.num as ConsecutiveNums
+from
+    Logs as l1 inner join Logs as l2
+    on l1.id = l2.id + 1
+    inner join Logs as l3
+    on l1.id = l3.id - 1
+where
+    l1.num = l2.num and l1.num = l3.num;
