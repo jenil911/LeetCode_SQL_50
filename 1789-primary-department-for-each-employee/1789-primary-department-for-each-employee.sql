d01b71d74da190b52a45dacbb58cb748a0c7d752
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-03-21 20:33:29 +0000

    Time: 448 ms (97.12%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,9 +1,11 @@
-# Write your MySQL query statement below
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-UNION
-SELECT employee_id, department_id
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(*) = 1;
+/* Write your T-SQL query statement below */
+select employee_id, department_id 
+from Employee
+where primary_flag = 'Y'
+
+union
+
+select employee_id, max(department_id)
+from Employee 
+group by employee_id 
+having count(employee_id) = 1;
