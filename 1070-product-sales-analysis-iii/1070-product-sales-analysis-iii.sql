169e7622125ba9371c2938d5f9c25a899547748e
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-03-28 04:03:10 +0000

    Time: 2829 ms (87.66%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,7 +1,9 @@
-# Write your MySQL query statement below
-select product_id, year as first_year, quantity, price 
-from sales where (product_id, year) in (
-    select product_id, min(year)
-    from sales
-    group by product_id
+/* Write your T-SQL query statement below */
+WITH CTE AS (
+    SELECT product_id, MIN(year) AS minyear FROM Sales 
+    GROUP BY product_id 
 )
+
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price 
+FROM Sales s
+INNER JOIN CTE ON cte.product_id = s.product_id  AND s.year = cte.minyear; 
