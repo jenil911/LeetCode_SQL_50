ca2ebedffa807989da7ae97e7592f8087f65632f
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-03-31 03:07:52 +0000

    Time: 1438 ms (93.29%), Space: 0B (100%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,5 @@
-
-Select  teacher_id ,
-count(distinct subject_id ) as cnt from Teacher
-group by teacher_id
+/* Write your T-SQL query statement below */
+SELECT TEACHER_ID ,
+ COUNT(DISTINCT SUBJECT_ID) AS CNT 
+ FROM TEACHER 
+ GROUP BY TEACHER_ID;
