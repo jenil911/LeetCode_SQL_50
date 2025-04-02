19365cc9280de73cf61e1e882cdcdeea1b124466
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2025-04-02 03:19:32 +0000

    Time: 880 ms (96.62%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,7 +1,35 @@
-SELECT round(avg( order_date = customer_pref_delivery_date ) * 100, 2) AS immediate_percentage
-FROM Delivery
-WHERE ( customer_id, order_date) in (
-    SELECT customer_id, min(order_date)
-    FROM Delivery
-    GROUP BY customer_id
-)
+/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */
+DECLARE @TotalCustomers int;
+SET @TotalCustomers = (
+    SELECT 
+        COUNT(*) 
+    FROM (
+            SELECT 
+                customer_id 
+            FROM 
+                Delivery 
+            GROUP BY 
+                customer_id
+        ) AS Temp
+    );
+
+
+DECLARE @ImmediateOrderCustomers int;
+SET @ImmediateOrderCustomers = (
+    SELECT 
+        COUNT(*) 
+    FROM (
+            SELECT 
+                customer_id 
+            FROM 
+                Delivery 
+            GROUP BY 
+                customer_id 
+            HAVING MIN(customer_pref_delivery_date) = MIN(order_date)) 
+        AS Temp
+    );
+
+
+SELECT 
+    ROUND(100.0 * @ImmediateOrderCustomers / @TotalCustomers, 2) AS immediate_percentage;
